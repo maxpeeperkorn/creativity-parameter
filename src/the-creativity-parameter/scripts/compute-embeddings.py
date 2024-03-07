2747d8d8f01ecbd623115d403ffff6ab1ff44c3d
Max Peeperkorn <max.peeperkorn@protonmail.com>
2024-03-07 16:11:38 +0000

    scripts to generate stories and compute embeddings

---
+++ src/the-creativity-parameter/scripts/compute-embeddings.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:adb1d8f0d0f7d89009a02e6827a2215ca6e5ad260d84becaf2011fefec106e71
+size 1810
---
+++ src/the-creativity-parameter/scripts/temp-scales.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:087c5f19f63a95ef1be353d5eaf3372d6f165e91857dcbc77f603d41ea26c1b4
+size 2720
