e408e0c177c29bf28bcee84fad81ac54ae9339f3
Max Peeperkorn <max.peeperkorn@protonmail.com>
2024-03-07 16:34:38 +0000

    added scripts to generate stories and compute embeddings

---
+++ scripts/compute-embeddings.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bf7274a0d0b6903414525073b50e4a679d2c3e64a11e559f5635bae57a5ba118
+size 1772
---
+++ scripts/temperatures.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7b5889a213c7359936faf76ef6b09264360f959ad06738917379d52fc4e83f28
+size 3388
